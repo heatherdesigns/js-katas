541032ff879906b873096a68e2bf32c98ed63ef7
Heather Taylor <heather@heysparkbox.com>
2017-04-03 13:20:02 -0400

    feat: update function and variable names

--- roman_numerals_v2.js
+++ roman_numerals_v2.js
@@ -1,110 +1,114 @@
 /*http://codingdojo.org/kata/RomanNumerals/
 * Version 2
-* March 23, 2017
+* April 3, 2017
 */
 
 function numberToRomanNumeral(number){
 
-	let num = number;
-	let result = "";
+	let roman_numeral = "";
 
-	maxNumberAllowed(number);
-	hasThousand(number);
-	hasHundreds(number);
-	hasTens(number);
-	hasOnes(number);
+	isOutsideRange(number);
+	let thousands = findThousands(number);
+	let hundreds = findHundreds(thousands);
+	let tens = findTens(hundreds);
+	findOnes(tens);
 
-	function maxNumberAllowed(number){
-		if (num > 9999) {
-			console.log("This program will only translate numbers equal to or less than 9999.");
-			console.log("Goodbye!");
+	function isOutsideRange(number){
+		if (number > 9999 || number <= 0) {
+			console.log(number + " is an unacceptable answer. This program will only translate numbers between 1 and 9999.");
 		}
 	}
 
-	function hasThousand(number){
-		while (num >= 1000 && num < 10000) {
-			result += "M";
-			num -= 1000;
+	function findThousands(number){
+		while (number >= 1000 && number < 10000) {
+			roman_numeral += "M";
+			number -= 1000;
 		}
+		return number;
 	}
 
-	function hasHundreds(number){
-		while (num < 1000 && num >= 100) {
-			if (num >= 900) {
-				result += "CM";
-				num -= 900;
+	function findHundreds(number){
+		while (number < 1000 && number >= 100) {
+			if (number >= 900) {
+				roman_numeral += "CM";
+				number -= 900;
 			}
 
-			else if (num >= 500) {
-				result += "D";
-				num -= 500;
+			else if (number >= 500) {
+				roman_numeral += "D";
+				number -= 500;
 			}
 
-			else if (num >= 400) {
-				result += "CD";
-				num -= 400;
+			else if (number >= 400) {
+				roman_numeral += "CD";
+				number -= 400;
 			}
 
-			else if ((num < 900 && num > 500) || (num < 400 && num >= 100)){
-				result += "C";
-				num -= 100;
-			}
-		}
-	}
-
-	function hasTens(number) {
-		while (num < 100 && num >= 10){
-			if (num >= 90) {
-				result += "XC";
-				num -= 90;
-			}
-
-			else if (num >= 50) {
-				result += "L";
-				num -= 50;
-			}
-
-			else if (num >= 40) {
-				result += "XL";
-				num -= 40;
-			}
-
-			else if ((num < 90 && num > 50) || (num < 40 && num >= 10)) {
-				result += "X";
-				num -= 10;
+			else if ((number < 900 && number > 500) || (number < 400 && number >= 100)){
+				roman_numeral += "C";
+				number -= 100;
 			}
 		}
+		return number;
 	}
 
-	function hasOnes(number) {
-		while (num < 10 && num >= 1) {
-			if (num >= 9) {
-				result += "IX";
-				num -= 9;
+	function findTens(number) {
+		while (number < 100 && number >= 10){
+			if (number >= 90) {
+				roman_numeral += "XC";
+				number -= 90;
 			}
 
-			else if (num >= 5) {
-				result += "V";
-				num -= 5;
+			else if (number >= 50) {
+				roman_numeral += "L";
+				number -= 50;
 			}
 
-			else if (num >= 4) {
-				result += "IV";
-				num -= 4;
+			else if (number >= 40) {
+				roman_numeral += "XL";
+				number -= 40;
 			}
 
-			else if ((num < 9 && num > 5) || (num < 4 && num >= 1)) {
-				result += "I";
-				num -= 1;
+			else if ((number < 90 && number > 50) || (number < 40 && number >= 10)) {
+				roman_numeral += "X";
+				number -= 10;
 			}
 		}
+		return number;
 	}
 
-	if (number < 10000) {
-		return console.log("The number " + number + " converts to the Roman Numeral " + result + ".");
+	function findOnes(number) {
+		while (number < 10 && number >= 1) {
+			if (number >= 9) {
+				roman_numeral += "IX";
+				number -= 9;
+			}
+
+			else if (number >= 5) {
+				roman_numeral += "V";
+				number -= 5;
+			}
+
+			else if (number >= 4) {
+				roman_numeral += "IV";
+				number -= 4;
+			}
+
+			else if ((number < 9 && number > 5) || (number < 4 && number >= 1)) {
+				roman_numeral += "I";
+				number -= 1;
+			}
+		}
+		return number;
+	}
+
+	if (number < 10000 && number > 0) {
+		return console.log("The number " + number + " converts to the Roman Numeral " + roman_numeral + ".");
 	}
 
 }
 
 numberToRomanNumeral(3856);
 numberToRomanNumeral(127);
+numberToRomanNumeral(0);
+numberToRomanNumeral(10567);
