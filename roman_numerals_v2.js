36cf0d00f06b0946353cd443f495df6ca8665616
Heather Taylor <heather@heysparkbox.com>
2017-04-05 17:46:32 -0400

    feat: eliminate global roman numeral variable
    eliminate global roman numeral variable and implement hashes which return a number and a roman numeral

--- roman_numerals_v2.js
+++ roman_numerals_v2.js
@@ -5,16 +5,13 @@
 
 function numberToRomanNumeral(number){ // make a class whose state is the current roman numeral
 
-	// working to eliminate this global variable
-	//let roman_numeral = "";
-
 	function isOutsideRange(number){
 		if (number > 9999 || number <= 0) {
 			console.log(number + " is an unacceptable answer. This program will only translate numbers between 1 and 9999.");
 		}
 	}
 
-	let thousands = function (number) {
+	let findThousands = function (number) {
 		let roman_numeral = "";
 		while (number >= 1000 && number < 10000) {
 			roman_numeral += "M";
@@ -29,7 +26,8 @@ function numberToRomanNumeral(number){ // make a class whose state is the curren
 		return numRomanSet;
 	};
 
-	let hundreds = (number) => {
+	let findHundreds = (number) => {
+		let roman_numeral = "";
 		while (number < 1000 && number >= 100) {
 			if (number >= 900) {
 				roman_numeral += "CM";
@@ -51,10 +49,17 @@ function numberToRomanNumeral(number){ // make a class whose state is the curren
 				number -= 100;
 			}
 		}
-		return number;
+		// create hash
+		let numRomanSet = { num: number, roman: roman_numeral };
+		// let's see what it looks like -- to be removed! --
+		console.log("The number after hundreds is: " + numRomanSet["num"]);
+		console.log("The roman numeral after hundreds is: " + numRomanSet["roman"]);
+		// return hash with number AND roman numeral
+		return numRomanSet;
 	};
 
-	let tens = (number) => {
+	let findTens = (number) => {
+		let roman_numeral = "";
 		while (number < 100 && number >= 10){
 			if (number >= 90) {
 				roman_numeral += "XC";
@@ -76,10 +81,17 @@ function numberToRomanNumeral(number){ // make a class whose state is the curren
 				number -= 10;
 			}
 		}
-		return number;
+		// create hash
+		let numRomanSet = { num: number, roman: roman_numeral };
+		// let's see what it looks like -- to be removed! --
+		console.log("The number after hundreds is: " + numRomanSet["num"]);
+		console.log("The roman numeral after hundreds is: " + numRomanSet["roman"]);
+		// return hash with number AND roman numeral
+		return numRomanSet;
 	};
 
-	let ones = (number) => {
+	let findOnes = (number) => {
+		let roman_numeral = "";
 		while (number < 10 && number >= 1) {
 			if (number >= 9) {
 				roman_numeral += "IX";
@@ -101,25 +113,44 @@ function numberToRomanNumeral(number){ // make a class whose state is the curren
 				number -= 1;
 			}
 		}
-		return number;
+		// create hash
+		let numRomanSet = { num: number, roman: roman_numeral };
+		// let's see what it looks like -- to be removed! --
+		console.log("The number after hundreds is: " + numRomanSet["num"]);
+		console.log("The roman numeral after hundreds is: " + numRomanSet["roman"]);
+		// return hash with number AND roman numeral
+		return numRomanSet;
 	};
 
-	isOutsideRange(number);
-	// need to access hash values (working it!)  -------------------------------------
-	let newNumber = thousands(number);
-	console.log(newNumber);
-	console.log(Object.keys(newNumber));
-	console.log(Object.values(newNumber));
-	console.log(Object.values(newNumber)[0]); // number!
-	console.log(Object.values(newNumber)[1]); // roman numeral!
+	// after section above is converted to a class, place the section below into a driver file
 
-	newNumber = hundreds(newNumber); // reassign and send into the next function
-	newNumber = tens(newNumber);
-	ones(newNumber);
+	isOutsideRange(number);
+
+	let thousands = findThousands(number);
+	console.log(Object.values(thousands)[0]); // number!
+	console.log(Object.values(thousands)[1]); // roman numeral!
+
+	let sendToHundreds = (Object.values(thousands)[0]); // assign number to send into the hundreds function
+
+	let hundreds = findHundreds(sendToHundreds);
+	console.log(Object.values(hundreds)[0]); // number!
+	console.log(Object.values(hundreds)[1]); // roman numeral!
+
+	let sendToTens = (Object.values(hundreds)[0]); // assign number to send into the tens function
+
+	let tens = findTens(sendToTens);
+	console.log(Object.values(tens)[0]); // number!
+	console.log(Object.values(tens)[1]); // roman numeral!
+
+	let sendToOnes = (Object.values(tens)[0]);
+
+	let ones = findOnes(sendToOnes);
 
 	if (number < 10000 && number > 0) {
 		// need to add roman numeral reference back to this statement.
-		return console.log("The number " + number + " converts to the Roman Numeral " + Object.values(newNumber)[1] + ".");
+		return console.log("The number " + number + " converts to the Roman Numeral "
+		+ Object.values(thousands)[1] + Object.values(hundreds)[1]
+		+ Object.values(tens)[1] + Object.values(ones)[1] + ".");
 	}
 
 }
